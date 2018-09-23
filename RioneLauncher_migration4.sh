66739010d7bbc928603e0c717839345d1549c6c6
MiglyA <zq6295migly@gmail.com>
2018-09-23 10:38:40 +0900

    RioneLauncher_migration4作成

--- RioneLauncher_migration4.sh
+++ RioneLauncher_migration4.sh
@@ -3,10 +3,14 @@
 #バージョン移行専用
 
 if [[ -f RioneLauncher.sh ]]; then
+
+	echo "しばらくお待ちください。。。"
+
+	rm $(find ~/ -type d -name ".*" -prune -o -type f -print | grep RioneLauncher.sh | grep boot)
 	
 	echo
 	echo "バージョンを6.00に移行します..."
-	wget -N -O RioneLauncher.sh `curl https://raw.githubusercontent.com/MiglyA/bash-rescue/master/histry.txt | grep "RioneLauncher5-link" | awk '{print $2}'` &>/dev/null
+	wget -N -O RioneLauncher.sh `curl https://raw.githubusercontent.com/Ri--one/bash-rescue/master/histry.txt | grep "RioneLauncher5-link" | awk '{print $2}'` &>/dev/null
 	rm score.log &>/dev/null
 	rm src.log &>/dev/null
 	rm server.log &>/dev/null
